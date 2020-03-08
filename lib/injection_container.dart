7b8d33417ea525b2d62fc8f72c40d03a3e2f5d1d
Mojave <mojave@Mojaves-iMac.local>
2020-03-08 22:40:25 +0300

    all done

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,5 +1,6 @@
 import 'package:hello_bloc_wallpaper/data/network/data_sources.dart';
 import 'package:hello_bloc_wallpaper/data/repository/video_repository.dart';
+import 'package:hello_bloc_wallpaper/ui/detail/detail_bloc.dart';
 import 'package:hello_bloc_wallpaper/ui/search/search_bloc.dart';
 import 'package:kiwi/kiwi.dart' as kiwi;
 import 'package:http/http.dart' as http;
@@ -8,5 +9,6 @@ void initKiwi(){
   kiwi.Container()..registerInstance(http.Client())
   ..registerFactory((container) => DataSource(container.resolve()))
   ..registerFactory((container) => VideoRepository(container.resolve()))
-  ..registerFactory((container) => SearchBloc(container.resolve()));
+  ..registerFactory((container) => SearchBloc(container.resolve()))
+  ..registerFactory((container) => DetailBloc(container.resolve()));
 }
--- lib/ui/search/search_page.dart
+++ lib/ui/search/search_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hello_bloc_wallpaper/data/model/search/model_search.dart';
+import 'package:hello_bloc_wallpaper/ui/detail/detail_page.dart';
 import 'package:hello_bloc_wallpaper/ui/search/search_bloc.dart';
 import 'package:hello_bloc_wallpaper/ui/search/widget/centered_message.dart';
 import 'package:kiwi/kiwi.dart' as kiwi;
@@ -66,11 +67,12 @@ class _SearchPageState extends State<SearchPage> {
         itemBuilder: (context, index){
           return index >= state.searchResults.length
           ? _buildLoaderListItem()
-          : _buildVideoListItemCard(state.searchResults[index].snippet);
+          : _buildVideoListItem(state.searchResults[index]);
         },
       ),
     );
   }
+  
   int _calculateListItemCount(SearchState state){
     if(state.hasReachedEndOfResults){
       return state.searchResults.length;
@@ -78,6 +80,7 @@ class _SearchPageState extends State<SearchPage> {
       return state.searchResults.length + 1;
     }
   }
+  
   bool _handleScrollNotification(ScrollNotification notification){
     if(notification is ScrollEndNotification &&
     _scrollController.position.extentAfter == 0){
@@ -85,26 +88,42 @@ class _SearchPageState extends State<SearchPage> {
     }
     return false;
   }
+
+  Widget _buildVideoListItem(SearchItem searchItem){
+    return GestureDetector(
+      child: _buildVideoListItemCard(searchItem.snippet),
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(
+          builder: (_){
+            return DetailPage(videoId: searchItem.id.videoId,);
+           }
+          )
+        );
+      },
+    );
+  }
+
+  Widget _buildVideoListItemCard(SearchSnippet videoSnippet){
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Image.network(videoSnippet.thumbnails.high.url, fit: BoxFit.cover,
+              )
+            ),
+            SizedBox(height: 5,),
+            Text(videoSnippet.title, maxLines: 1, textScaleFactor: 1.1, style: TextStyle(fontWeight: FontWeight.bold), textAlign: TextAlign.start, overflow: TextOverflow.ellipsis,)
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-Widget _buildVideoListItemCard(SearchSnippet videoSnippet){
-  return Card(
-    child: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: <Widget>[
-          AspectRatio(
-            aspectRatio: 16 / 9,
-            child: Image.network(videoSnippet.thumbnails.high.url, fit: BoxFit.cover,
-            )
-          ),
-          SizedBox(height: 5,),
-          Text(videoSnippet.title, maxLines: 1, textScaleFactor: 1.1, style: TextStyle(fontWeight: FontWeight.bold), textAlign: TextAlign.start, overflow: TextOverflow.ellipsis,)
-        ],
-      ),
-    ),
-  );
-}
+
 
 Widget _buildLoaderListItem(){
   return Center(
